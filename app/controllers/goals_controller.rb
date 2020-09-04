b83a28e9f9fd9f3a758c30c82baf9fb7ce2fb15f
syota-tominaga <68591369+syota-tominaga@users.noreply.github.com>
2020-09-04 19:16:35 +0900

    aa

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,20 @@
 class GoalsController < ApplicationController
-  def index
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      redirect_to memory_goals_path
+    else
+      binding.pry
+      render 'new'
+    end
+  end
+
+  private
+  def goal_params
+    params.permit(:goal_money, :goal_goods, :memory_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'memories#index'
   resources :memories, only: [:index, :new, :create] do
-    resources :goals, only: [:index, :new, :create]
+    resources :goals, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
