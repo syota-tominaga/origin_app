444107720e57d9a071b465c06edd4c36d16f3c32
syota-tominaga <68591369+syota-tominaga@users.noreply.github.com>
2020-09-08 13:00:58 +0900

    tweet機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,20 @@
 class TweetsController < ApplicationController
   def new
-    @goal = Goal.find(params[:id])
+    @goal = Goal.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.permit(:title, :text, :image, :goal_id)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,4 @@
 class Tweet < ApplicationRecord
   belongs_to :goal
+  has_one_attached :image
 end
